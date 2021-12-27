aa0c9e25e25fba3673f7e000270c7d503e79f10c
Binit <cruiserponder@protonmail.com>
2021-12-27 18:17:36 +0545

    some validation

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -4,17 +4,25 @@ import { store } from "../stores/auth.js";
 let password = "";
 let email = "";
 let loading = null;
+let error = { email: "", password: "" };
 import axios from "axios";
 
 const onLogin = () => {
-  if (password.length < 6) {
-    alert("Password must be at least 6 characters long");
-    return;
+  if (!email || !password) {
+    error.email = "Email is required";
+    error.password = "Password is required";
   }
   if (!email.includes("@")) {
-    alert("Enter valid Email");
+    error.email = "Email is invalid";
+  }
+  if (password.length < 6) {
+    error.password = "Password must be at least 8 characters";
+  }
+  if (error.email || error.password) {
     return;
   }
+
+  loading = true;
   axios
     .post("https://recipetohome-api.herokuapp.com/api/auth/login", {
       email: email,
@@ -42,13 +50,29 @@ const onLogin = () => {
 <h1>Login Screen</h1>
 
 <form action={onLogin} method="post">
-  <input bind:value={email} type="text" name="email" placeholder="email" />
+  <input
+    bind:value={email}
+    type="text"
+    name="email"
+    placeholder="email"
+    on:focus={() => {
+      error.email = "";
+    }} />
+  <p hidden={!error.email} style="color:red">
+    {error.email ? error.email : " "}
+  </p>
   <input
     bind:value={password}
     type="password"
     name="password"
-    placeholder="Password" />
-  <button on:click|preventDefault={onLogin}
+    placeholder="Password"
+    on:focus={() => {
+      error.password = "";
+    }} />
+  <p style="color:red" hidden={!error.password}>
+    {error.password ? error.password : " "}
+  </p>
+  <button on:click|preventDefault={onLogin} disabled={loading}
     >{loading ? "Loading" : "Login"}</button>
 </form>
 
@@ -57,5 +81,9 @@ form {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+p {
+  margin: 0;
+  margin-bottom: 0.25em;
 }
 </style>
